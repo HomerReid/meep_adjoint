d8467cf610d4b843600752e6a4da404c0c92ff9e
homerreid <homereid2@gmail.com>
2019-10-07 16:31:34 -0400

    updates

--- docs/PolishHTML.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-BASE=/home/homer/work/Simpetus/meep_adjoint/docs/_build/html
-
-#for FILE in `find ${BASE} -name '*html'`; do
-FILE=${BASE}/Tutorial/index.html
-  sed -i 's/\(toctree-l1.*\)meep_adjoint\(.*\)MEEP/\1<code xref>meep_adjoint<\/code>\2<span class="codename">meep<\/span>/g' ${FILE}
-#done
